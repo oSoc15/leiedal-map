417a670a01b91ddbc9813c1f0235b12a54e006f2
Mithos-Yggdrasill <pieter_declercq@hotmail.com>
2015-07-29 14:48:36 +0200

    Added info-searching functionality

--- test.php
+++ test.php
@@ -5,12 +5,41 @@ if (!empty($_GET['location'])) {
     $maps_json = file_get_contents($maps_url);
     $maps_array = json_decode($maps_json, true);
 
-    $lat = $maps_array['LocationResult'][0]['Location']['Lat_WGS84'];
-    $lon = $maps_array['LocationResult'][0]['Location']['Lon_WGS84'];
-    $zoom = 18;
+    if (!empty($maps_array['LocationResult'][0]['Location']['Lat_WGS84'])) {
+        $lat = $maps_array['LocationResult'][0]['Location']['Lat_WGS84'];
+        $lon = $maps_array['LocationResult'][0]['Location']['Lon_WGS84'];
+        $zoom = 18;
 
-    //$google_maps_url = 'https://www.google.com/maps/@' . $lat . ',' . $lon . ',' . $zoom . 'z';
-    //header("Location: $google_maps_url");
+
+        $formattedAdress = $maps_array['LocationResult'][0]['FormattedAddress'];
+        $keywords = preg_split("/[,]+/", $formattedAdress);
+        $length = count($keywords);
+        $adres = $keywords[0];
+
+        if (ctype_alpha(substr($adres, -1))) {
+            $adres = substr($adres, 0, strlen($adres) - 1) . '  ' . substr($adres, -1);
+        }
+
+        $obid_json = file_get_contents('http://www.govmaps.eu/arcgis/rest/services/ICL/ICL_Energielabelatlas/MapServer/0/query?where=ADRES' . urlencode('=\'' . $adres . '\'') . '&returnIdsOnly=true&f=pjson');
+
+        $obid_array = json_decode($obid_json, true);
+
+        $objectId = $obid_array['objectIds'][0];
+
+        $residence_info_json = file_get_contents("http://www.govmaps.eu/arcgis/rest/services/ICL/ICL_Energielabelatlas/MapServer/2/" . $objectId . "?f=pjson");
+        $residence_info_array = json_decode($residence_info_json, true);
+
+        if (!empty($residence_info_array['feature']['attributes'])) {
+            echo var_dump($residence_info_array['feature']['attributes']);
+        } else {
+            echo 'no info over this house';
+        }
+    } else {
+        echo 'this house is not found';
+        $lat = 50.8086126;
+        $lon = 3.2469894;
+        $zoom = 10;
+    }
 } else {
     $lat = 50.8086126;
     $lon = 3.2469894;
@@ -37,6 +66,7 @@ if (!empty($_GET['location'])) {
     </head>
     <body>
         <a href="arcgis.php">energylabelatlas</a>
+        <a href="#">bereken label</a>
         <form action="">
             <input type="text" name="location" />
             <button type="submit">submit</button>
